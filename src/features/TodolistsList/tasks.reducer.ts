e4e1e7356e92a3e6c7c96a7cdaef18c5c75dded6
apelsinom <apelsinom@mail.ru>
2024-03-14 20:44:21 +0300

    refactor fetchTasks and handleServerNetworkError

--- src/features/TodolistsList/tasks.reducer.ts
+++ src/features/TodolistsList/tasks.reducer.ts
@@ -5,6 +5,7 @@ import { appActions } from "app/app.reducer"
 import { todolistsActions } from "features/TodolistsList/todolists.reducer"
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { clearTasksAndTodolists } from "common/actions/common.actions"
+import { createAppAsyncThunk } from "../../utils/createAppAsyncThunk"
 
 const initialState: TasksStateType = {}
 
@@ -35,15 +36,19 @@ const slice = createSlice({
         tasks[index] = { ...tasks[index], ...action.payload.model }
       }
     },
-    setTasks: (state, action: PayloadAction<{ tasks: Array<TaskType>; todolistId: string }>) => {
+    /*    setTasks: (state, action: PayloadAction<{ tasks: Array<TaskType>; todolistId: string }>) => {
       state[action.payload.todolistId] = action.payload.tasks
-    },
+    },*/
     /*    clearTasks: () => {
       return {}
     },*/
   },
   extraReducers: (builder) => {
     builder
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state[action.payload.todolistId] = action.payload.tasks
+      })
+      // .addCase(fetchTasks.rejected, (state, action) => {})
       .addCase(todolistsActions.addTodolist, (state, action) => {
         state[action.payload.todolist.id] = []
       })
@@ -60,21 +65,26 @@ const slice = createSlice({
       })
   },
 })
-
-export const tasksReducer = slice.reducer
-export const tasksActions = slice.actions
+// thunks
+const fetchTasks = createAppAsyncThunk<{ tasks: TaskType[]; todolistId: string }, string>(
+  `${slice.name}/fetchTasks`,
+  async (todolistId, thunkAPI) => {
+    const { dispatch, rejectWithValue } = thunkAPI
+    try {
+      dispatch(appActions.setAppStatus({ status: "loading" }))
+      const res = await todolistsAPI.getTasks(todolistId)
+      const tasks = res.data.items
+      dispatch(appActions.setAppStatus({ status: "succeeded" }))
+      return { tasks, todolistId }
+    } catch (e: any) {
+      console.log(e.message)
+      handleServerNetworkError(e, dispatch)
+      return rejectWithValue(null)
+    }
+  },
+)
 
 // thunks
-export const fetchTasksTC =
-  (todolistId: string): AppThunk =>
-  (dispatch) => {
-    dispatch(appActions.setAppStatus({ status: "loading" }))
-    todolistsAPI.getTasks(todolistId).then((res) => {
-      const tasks = res.data.items
-      dispatch(tasksActions.setTasks({ tasks, todolistId }))
-      dispatch(appActions.setAppStatus({ status: "succeeded" }))
-    })
-  }
 
 export const removeTaskTC =
   (taskId: string, todolistId: string): AppThunk =>
@@ -150,3 +160,6 @@ export type UpdateDomainTaskModelType = {
 export type TasksStateType = {
   [key: string]: Array<TaskType>
 }
+export const tasksReducer = slice.reducer
+export const tasksActions = slice.actions
+export const tasksThunks = { fetchTasks }
